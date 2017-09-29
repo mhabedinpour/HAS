08ffafd9c8d2d6dcf62f2ea7bf7b794a17a48de0
robinbolscher@gmail.com <robinbolscher@gmail.com>
2017-09-29 19:34:32 +0200

    Update data.json and generate predefined.ts (this adds e.g. ColorTemperature)

--- src/TCP.d.ts
+++ src/TCP.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="node" />
 import { EventEmitter } from 'events';
 import HAS from './HAS';
 export default class TCP extends EventEmitter {
--- src/express.d.ts
+++ src/express.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="express" />
 import * as express from 'express';
 import HAS from './HAS';
 export default function (server: HAS): express.Express;
